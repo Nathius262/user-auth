ca8dd424bdfb409ca2ff62762893e312458dd9e7
Nathaniel Odo <egbodonathaniel@yahoo.com>
2024-12-06 11:58:39 +0100

    i

--- middlewares/eventLogger.js
+++ middlewares/eventLogger.js
@@ -2,7 +2,7 @@ export const logger = async (req, res, next) => {
     const start = Date.now();
     res.on('finish', ()=>{
         const duration = Date.now() - start;
-        console.log(`[${new Date().toISOString}] ${req.method} ${req.url} ${res.statusCode} time- ${duration}ms`)
+        console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} ${res.statusCode} time- ${duration}ms`)
     })
     next();
 }
