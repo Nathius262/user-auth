2280db46f2d2aff4a658fb5c4ab1e54d7773b871
Nathaniel Odo <egbodonathaniel@yahoo.com>
2024-12-06 11:39:25 +0100

    updated app

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
 
+import { logger } from './middlewares/eventLogger.js';
 import authRoute from './routers/auth.js';
 
+
+
+dotenv.config();
 
 const app =express();
 
 
+const PORT = process.env.PORT || 3000
+
 //basic configuration
-app.use(bodyParser.urlencoded({extended:tru}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(logger)
+
+app.use('/auth', authRoute);
+
+
+app.listen(PORT, () => {
+    console.log('Server Started at Port '+PORT)
+});
---
+++ middlewares/eventLogger.js
@@ -0,0 +1,8 @@
+export const logger = async (req, res, next) => {
+    const start = Date.now();
+    res.on('finish', ()=>{
+        const duration = Date.now() - start;
+        console.log(`[${new Date().toISOString}] ${req.method} ${req.url} ${res.statusCode} time- ${duration}ms`)
+    })
+    next();
+}
